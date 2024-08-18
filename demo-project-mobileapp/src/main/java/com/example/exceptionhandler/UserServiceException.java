13d5130c1eb60a4f7435075c968157377718d3ab
ghanshyam shukla <gsgmca682@gmail.com>
2024-08-18 08:27:36 +0530

    code commited for the kafka broker and consumer-remove test-testcontainer

--- demo-project-mobileapp/src/main/java/com/example/exceptionhandler/UserServiceException.java
+++
@@ -1,9 +0,0 @@
-package com.example.exceptionhandler;
-
-public class UserServiceException extends RuntimeException{
-
-    public UserServiceException(String message)
-    {
-        super(message);
-    }
-}
--- demo-project-mobileapp/src/main/java/com/example/service/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.example.service;
-
-import com.example.model.request.UserRequest;
-
-public interface UserService {
-public UserRequest createUser(UserRequest request);
-}
